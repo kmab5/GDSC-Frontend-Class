54dda68a9478b9fc6fe3151c759f3748a926c245
Samuel Worash Getaneh <samuelworash@gmail.com>
2024-07-04 20:55:06 +0300

    some polishing touches

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const drawer = document.querySelector(".dropdown");
 const navbar = document.querySelector(".navbar");
+const form = document.querySelector("#messageform");
+
+form.addEventListener("submit", e => {
+    console.log(e);
+    submit();
+    e.preventDefault();
+});
 
 // Highlight page
 window.onload = e => {
     console.log("Loaded");
     if(document.location.toString().match(/home/g)){
-        Array.from(document.querySelectorAll("nav.navbar .rightnav p"))[0].style.setProperty("color", "darkgreen");
+        Array.from(document.querySelectorAll("nav.navbar .rightnav p"))[0].style.setProperty("color", "var(--col)");
         console.log(`${document.location}: Home`);
     } else if(document.location.toString().match(/service/g)){
-        Array.from(document.querySelectorAll("nav.navbar .rightnav p"))[1].style.setProperty("color", "darkgreen");
+        Array.from(document.querySelectorAll("nav.navbar .rightnav p"))[1].style.setProperty("color", "var(--col)");
         console.log(`${document.location}: Service`);
     } else if(document.location.toString().match(/index/g)){
-        Array.from(document.querySelectorAll("nav.navbar .rightnav p"))[2].style.setProperty("color", "darkgreen");
+        Array.from(document.querySelectorAll("nav.navbar .rightnav p"))[2].style.setProperty("color", "var(--col)");
         console.log(`${document.location}: Contact`);
     }
 };
@@ -22,3 +29,7 @@ document.querySelector("#drawericon").addEventListener("click", e => {
     navbar.classList.toggle("active");
 });
 
+
+function submit(){
+    alert("Message Sent! We will try to reach you as soon as possible!");
+}
